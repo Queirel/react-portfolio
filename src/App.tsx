39936efb21fe9aae30200a652e62e9f4332866e1
Fede <federicoqueirel@hotmail.com>
2024-07-02 02:59:12 -0300

    navbar-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <div className={styles.App}>Hello World</div>
+    <div className={styles.App}><Navbar></Navbar></div>
   )
 }
 
